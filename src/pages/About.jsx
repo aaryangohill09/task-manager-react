5aceb32cd51a56a3921629f4379f0954b67ae9b0
aaryangohill09 <aaryangohill09@gmail.com>
2026-09-25 18:47:58 +0530

    Update complete React Task Manager project

---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>This Task Manager is built with React.</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/pages/Tasks.jsx
@@ -0,0 +1,10 @@
+function Tasks() {
+  return (
+    <div>
+      <h2>Tasks</h2>
+      <p>Your tasks will appear here.</p>
+    </div>
+  );
+}
+
+export default Tasks;
